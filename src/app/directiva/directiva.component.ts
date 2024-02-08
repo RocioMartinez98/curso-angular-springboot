2144555c0a0ec4df751ff46831b82c51ca32c007
Rocio Martinez <rocioabigailmartinez98@gmail.com>
2024-02-08 10:58:27 -0300

    sección 2

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+
+export class DirectivaComponent {
+  listaCurso: string[] = ['TypeScript', 'JavaScript', 'Java SE', 'PHP']
+  habilitar: boolean = true;
+
+  constructor(){
+
+  }
+
+  estadoBoton() : void{
+  this.habilitar= (this.habilitar==true)? false : true
+
+  }
+
+}
